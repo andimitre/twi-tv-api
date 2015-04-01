7abb7f9e8d4926392eda9211aac2e02b526240ff
andi <amitre@pegasus.rutgers.edu>
2015-04-01 12:57:34 -0400

    deleting tv.py file

--- dbconn.php
+++ dbconn.php
@@ -1,29 +1,26 @@
 <?php
-// instantiate the driver class
-$mongo = new MongoClient();
-echo "Starting Connection!";
+ 
+    // Show PHP errors (during development only)
+    error_reporting(E_ALL | E_STRICT);
+    ini_set("display_errors", 2);
+ 
+    // Create a Mongo conenction
+    $mongo = new Mongo("mongodb://localhost");
+ 
+    // Choose the database and collection
+    $db = $mongo->my_db_name;
+    $coll = $db->my_collection_name;
+ 
+    // Same a document to the collection
+    $coll->save(array(
+        "name" => "Jack Sparrow",
+        "age" => 34,
+        "occupation" => "Pirate"
+    ));
+ 
+    // Retrieve the document and display it
+    $item = $coll->findOne();
+ 
+    echo "My name is " . $item['name'] . ". I am " . $item['age'] . " years old and work full-time as a " . $item['occupation'] . ".";
 
-//select a database
-$db = $mongo->test;
-echo "Test database selected!";
-
-// create a collection
-
-$collection = $db->createCollection('student');
-echo "Collection 'student' created";
-
-// insert a document into collection
-
-$document = array(
-		"name" => "andi",
-		"school" => "Rutgers",
-		"gpa" => 3.5
-	);
-
-
-$collection->insert($document);
-echo "Document inserted into collection";
-
-// mongodb conn
-?> 
-
+    ?>
